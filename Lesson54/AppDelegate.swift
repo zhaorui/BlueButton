c21e1834c12acf2da6b6d533fc38570ff987b341
zhaorui <zhaoruiexplorer@icloud.com>
2017-01-21 17:58:51 +0800

    Initial Commit

---
+++ Lesson54/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Lesson54
+//
+//  Created by 赵睿 on 21/01/2017.
+//  Copyright © 2017 com.homebrew.zhaorui. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Lesson54/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Lesson54
+//
+//  Created by 赵睿 on 21/01/2017.
+//  Copyright © 2017 com.homebrew.zhaorui. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
